810d233283f6cc4267c9d55674cc59362d32addc
Pablo Ortega <pablo@novaviz.com>
2018-08-09 17:34:13 -0400

    Rolling back model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -12,11 +12,4 @@
 
 ActiveRecord::Schema.define(version: 2018_08_09_211825) do
 
-  create_table "blogs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "title"
-    t.string "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
 end
