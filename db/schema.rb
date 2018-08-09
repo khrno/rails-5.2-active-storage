8e462c376ac638e7217e03763373dfa5d726b701
Pablo Ortega <pablo@novaviz.com>
2018-08-09 17:37:25 -0400

    Adding Categories model-

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_09_211825) do
+ActiveRecord::Schema.define(version: 2018_08_09_213539) do
+
+  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
