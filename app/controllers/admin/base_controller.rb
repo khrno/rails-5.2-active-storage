86460eb19470ca7b6c68cea5b6c0e45a7d3a1cdd
Pablo Ortega <pablo@novaviz.com>
2018-08-09 17:57:23 -0400

    Adding basic authentication to Admin/BaseController

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,2 +1,16 @@
 class Admin::BaseController < ApplicationController
+  before_action :authenticate
+
+  private
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      if Rails.env == 'development'
+        Rails.logger.debug 'ACA'
+        username == 'root'
+      else
+        username == 'root' && password == 'BpVmN0oS%10ioBF'
+      end
+    end
+  end
 end
