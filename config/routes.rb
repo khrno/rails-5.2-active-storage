32ff76458d5b1a4c6e31e6bcd093a923e4b3e762
Pablo Ortega <pablo@novaviz.com>
2018-08-12 16:59:06 -0300

    Adding new form to create a category

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :admin do
     root 'dashboard#index'
-    resources :categories, only: :index
+    resources :categories, only: [:index, :new, :create]
     resources :posts, only: :index
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
