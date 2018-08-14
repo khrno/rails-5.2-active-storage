def9fc477a21df090f20e21f81a4f5fdc72b788e
Pablo Ortega <pablo@novaviz.com>
2018-08-14 00:06:15 -0300

    Adding routes to edit, update and delete

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :admin do
     root 'dashboard#index'
     resources :categories, except: :show
-    resources :posts, only: [:index, :new, :create]
+    resources :posts, except: :show
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
